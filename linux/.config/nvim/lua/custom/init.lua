edaa13760ccdf90efb1f36a605fc5b9d4e66d066
Luis Tupa <luistupa7@gmail.colm>
2023-08-01 10:59:54 -0500

    feat: add nvim config for linux OS

---
+++ linux/.config/nvim/lua/custom/init.lua
@@ -0,0 +1,8 @@
+vim.g.dap_virtual_text = true
+vim.opt.colorcolumn = "80"
+
+-- Disable persistent undo
+vim.opt.undofile = false
+
+-- clipboard
+vim.opt.clipboard:append("unnamedplus")
---
+++ linux/.config/nvim/lua/custom/plugins.lua
@@ -0,0 +1,22 @@
+local plugins = {
+  {
+    "vim-crystal/vim-crystal",
+    ft = "crystal",
+    config = function (_)
+      vim.g.crystal_auto_format = 1
+    end
+  },
+  {
+    "neovim/nvim-lspconfig",
+    config = function ()
+      require "plugins.configs.lspconfig"
+      require "custom.configs.lspconfig"
+    end
+  },
+  {
+    "christoomey/vim-tmux-navigator",
+    lazy = false
+  }
+}
+
+return plugins
